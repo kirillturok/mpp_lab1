d89ffa66b413d7a423539e78268aeb35aebe0e93
Kiryl <kirillturok1@gmail.com>
2021-09-08 21:57:57 +0300

    Добавьте файлы проекта.

---
+++ mpp_lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mpp_lab1
+{
+
+    struct TraceResult
+    {
+        string methodName;
+        string className;
+        long time;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет");
+            string str = Console.ReadLine();
+            Console.WriteLine("sbdfbd|||{0}|||fbd",str);
+            Console.ReadKey(true);
+        }
+    }
+}
